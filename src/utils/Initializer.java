f6824bd6994541c7c443f27b23ee86dac4e54bd2
José Rocha <48589830+joserocha94@users.noreply.github.com>
2019-05-07 23:24:49 +0100

    XML, Initializer comments + Initializer full constructor

--- src/utils/Initializer.java
+++ src/utils/Initializer.java
@@ -1,35 +1,204 @@
 package utils;
 
 import java.util.ArrayList;
-import program.Edge;
+import graphHandler.Graph;
+import ants.PheromonedEdge;
+import java.lang.reflect.Field;
+
+/**
+ * Classe Initializer : implementa o objecto criado a partir do parse do documento XML,
+ * sendo posteriormente utilizado para construir o Grafo e instanciar as propriedades do
+ * sistema na classe AntSimulator. 
+ * 
+ * @author Alexandre Filipe, Sofia Salgueiro, José Rocha
+ * @since 07-05-2019
+ */
 
 public class Initializer {
 
-	ArrayList<Edge> Edges;
+	ArrayList<PheromonedEdge> edges;
 	
-	Double pLevel;
-	Double FinalInst;
-	Integer AntColSize;
+	float pLevel;
+	float finalInst;
+	int antColSize;
+	int nbNode;
+	int nestNode;
+	float alpha, beta, delta, eta, rho;
 	
+	/**
+	 * Initializer construtor default.
+	 */
 	public Initializer() {}
 	
-	public Initializer(ArrayList<Edge> Edges, double pLevel, double FinalInst, int AntColSize) {
+	/**
+	 * Initializer contrutor - inicializa os atributos da classe
+	 * @param pLevel - atributo propagation level
+	 * @param finalInst - atributo instante final
+	 * @param antColSize - atributo nrº de elemento da colónia
+	 * @param nbNode - atributo nbnode
+	 * @param nestNode - atributo nestNode
+	 * @param alpha - atributo alpha
+	 * @param beta - atributo beta
+	 * @param delta - atributo delta
+	 * @param eta - atributo eta
+	 * @param rho - atributo rho 
+	 * @return Initializer - Objecto Initializer utilizado para construir o sistema de simulação
+	 */
+	public Initializer(ArrayList<PheromonedEdge> edges, float pLevel, float finalInst, int antColSize, int nbNode, int nestNode,
+			float alpha, float beta, float delta, float eta, float rho) {
 		this.pLevel = pLevel;
-		this.FinalInst = FinalInst;
-		this.AntColSize = AntColSize;
-		this.Edges = Edges;
+		this.finalInst = finalInst;
+		this.antColSize = antColSize;
+		this.edges = edges;
+		this.alpha = alpha;
+		this.beta = beta;
+		this.delta = delta;
+		this.eta = eta;
+		this.rho = rho;
+		this.nestNode = nestNode;
+		this.nbNode = nbNode;
 	}
-
+	
+	/**
+	 * Graph CreateGraph - devolve um Grafo a partir da ArrayList de arestas e do nbNode
+	 * @param nbNode - atributo nbNode
+	 * @param edges - atributo edges 
+	 * @return Graph 
+	 */
+	public Graph CreateGraph() {	
+		Graph g = new Graph(nbNode, edges);
+		return g;
+	}
+	
+	/**
+	 * Initializer toString - devolve uma String com os atributos da classe
+	 */
 	public String toString()
 	{
-		String output = "";
-		output = "[pLevel]: " + this.pLevel + "\n[FinalInst]: " + this.FinalInst + "\n[AntColSize]: " + this.AntColSize + "\n\n";
+		StringBuilder output = new StringBuilder();
+		String newline = System.getProperty("line.separator");
 		
-		for(int i=0; i<Edges.size(); i++) 
+		Field[] fields = this.getClass().getDeclaredFields();
+		
+		//PRINT FIELDS
+		for(Field field : fields)
 		{
-			output += this.Edges.get(i).toString() + "\n";
+			try 
+			{
+				if (field.getName() != "edges")
+				{
+					output.append("[" + field.getName().toUpperCase() + "]: " + field.get(this) + newline);
+				}
+			} 
+			catch (IllegalArgumentException e) { e.printStackTrace(); } 
+			catch (IllegalAccessException e) { e.printStackTrace(); }
 		}
 		
-		return output;
+		output.append(newline);
+		
+		//PRINT EDGES
+		for(int i=0; i<edges.size(); i++) 
+		{
+			output.append(this.edges.get(i).toString() + newline);
+		}
+		
+		return output.toString();
 	}
+	
+	/**
+	 * Initializer getNbNode() - devolve o atributo nbNode
+	 * @return nbNode 
+	 */
+	public int getNbNode() {
+		return nbNode;
+	}
+	
+	/**
+	 * Initializer getEdges() - devolve a ArrayList de Edges
+	 * @return edges 
+	 */
+	public ArrayList<PheromonedEdge> getEdges(){
+		return edges;
+	}
+	
+	/**
+	 * Initializer getPLevel() - devolve o atributo pLevel
+	 * @return pLevel 
+	 */	
+	public float getPLevel() {
+		return pLevel;
+	}
+	
+	/**
+	 * Initializer getFinalInst() - devolve o atributo finalInst
+	 * @return finalInst 
+	 */		
+	public float getFinalInst() {
+		return finalInst;
+	}
+
+	/**
+	 * Initializer getAntColSize() - devolve o atributo antColSize
+	 * @return antColSize 
+	 */	
+	public int getAntColSize() {
+		return antColSize;
+	}
+
+	/**
+	 * Initializer getNestNode() - devolve o atributo nestNode
+	 * @return nestNode 
+	 */	
+	public int getNestNode() {
+		return nestNode;
+	}
+
+	/**
+	 * Initializer getpLevel() - devolve o atributo pLevel
+	 * @return pLevel 
+	 */	
+	public float getpLevel() {
+		return pLevel;
+	}
+
+	/**
+	 * Initializer getAlpha() - devolve o atributo alpha
+	 * @return alpha 
+	 */	
+	public float getAlpha() {
+		return alpha;
+	}
+
+	/**
+	 * Initializer getBeta() - devolve o atributo beta
+	 * @return beta 
+	 */	
+	public float getBeta() {
+		return beta;
+	}
+
+	/**
+	 * Initializer getDelta() - devolve o atributo delta
+	 * @return delta 
+	 */	
+	public float getDelta() {
+		return delta;
+	}
+
+	/**
+	 * Initializer getEta() - devolve o atributo eta
+	 * @return eta 
+	 */	
+	public float getEta() {
+		return eta;
+	}
+
+	/**
+	 * Initializer getRho() - devolve o atributo rho
+	 * @return rho 
+	 */	
+	public float getRho() {
+		return rho;
+	}
+		
 }
--- src/utils/XML.java
+++ src/utils/XML.java
@@ -8,61 +8,260 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import program.Edge;
+import ants.PheromonedEdge;
+
+/**
+ * Classe XML : parse do doxumento XML. A partir do ficheiro disponilizado, será
+ * instanciada a classe Initializer que posteriormente será utilizada para construir
+ * o sistema de simulação
+ *  
+ * @author Alexandre Filipe, Sofia Salgueiro, José Rocha
+ * @since 07-05-2019
+ */
 
 public class XML
-{
-    public double TIME_END;
-	public int ANT_COL_SIZE;
-	public boolean P_LEVEL;	
-
+{		
+	
+	/**
+	 * Initializer LoadXML() - devolve um objeto do tipo Initializer com os atributos obtidos através do ficheiro XML.
+	 * @return Initializer 
+	 */
 	public static Initializer LoadXML(String xml_path) throws Exception 
 	{
 
 		String path = xml_path; 
+		Initializer init = new Initializer();
 		
 		try {
 			
+			//LOAD XML
 			DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = fact.newDocumentBuilder();
 			org.w3c.dom.Document doc = builder.parse(new File(path));
- 
-			Initializer init = new Initializer();
-			
-			// get common data
-			NodeList header = doc.getElementsByTagName("simulation");
+ 			
+			// GET SIMULATION DATA
+			NodeList header = getNodeList("simulation", doc);
 			Node node = header.item(0);
+			init.antColSize = getAntColSize(node);
+			init.pLevel = getpLevel(node);
+			init.finalInst = getFinalInst(node);
 			
-			init.AntColSize = Integer.parseInt(node.getAttributes().getNamedItem("antcolsize").getNodeValue());
-			init.pLevel = Double.valueOf(node.getAttributes().getNamedItem("plevel").getNodeValue());
-			init.FinalInst = Double.valueOf(node.getAttributes().getNamedItem("finalinst").getNodeValue());
-						
-			//get edges
-			ArrayList<Edge> edges = new ArrayList<Edge>();		
-			NodeList nodes = doc.getElementsByTagName("weight");
+			//GET GRAPH NODES
+			NodeList graphInfo = getNodeList("graph", doc); 
+			Node nodeG = graphInfo.item(0);
+			init.nbNode = getnbNodes(nodeG);
+			init.nestNode = getNestNode(nodeG); 
+					
+			//GET EDGES
+			ArrayList<PheromonedEdge> edges = new ArrayList<PheromonedEdge>();		
+			NodeList nodes = getNodeList("weight", doc);
 			
 			for(int i=0; i< nodes.getLength(); i++) {
 				
 				Node nodeA = nodes.item(i);
 				Node nodeB = nodeA.getParentNode();
 								
-				Edge e = new Edge(
-					Integer.parseInt(nodeB.getAttributes().getNamedItem("nodeidx").getNodeValue()),
-					Integer.parseInt(nodeA.getAttributes().getNamedItem("targetnode").getNodeValue()),
-					Integer.parseInt(nodeA.getTextContent()),
-					0
+				PheromonedEdge e = new PheromonedEdge(
+					getNodeIDX(nodeB),
+					getTargetNode(nodeA),
+					getEdgeWeight(nodeA)
 				);
-				
 				edges.add(e);
 			}
 			
-			init.Edges = edges;
+			init.edges = edges;			
+			
+			//GET EVENTOS			
+			//MOVE
+			NodeList moveInfo = getNodeList("move", doc);
+			Node nodeM = moveInfo.item(0);
+			init.alpha = getAlpha(nodeM); 	
+			init.beta = getBeta(nodeM); 
+			init.delta = getDelta(nodeM);
+			
+			//EVAPORATION
+			NodeList evapInfo = getNodeList("evaporation", doc);
+			Node nodeE = evapInfo.item(0);
+			init.eta = getETA(nodeE);
+			init.rho = getRHO(nodeE);
 			
 			return init;						
 		}
 		catch (Exception e) { 
-			throw new Exception(e);
+			throw new Exception("PARSER - GRAFO INVALIDO: " + e);
 		} 
+	}
+	
+	/**
+	 * Initializer getAntColSize() - devolve o atributo antcolsize. Se o valor encontrado for negativo lança uma excepção.
+	 * @param node - no a desserializar
+	 * @return antcolsize  - atributo antcolsize
+	 */
+	public static int getAntColSize(Node node) throws Exception 
+	{
+		int antcolsize = Integer.parseInt(getNodeValue("antcolsize", node));		
+		if (antcolsize < 0) 
+		{ 
+			throw new Exception ("antcolsize must be >= 0"); 
+		}		
+		return antcolsize;
+	}
+
+	/**
+	 * Initializer getpLevel() - devolve o atributo plevel.
+	 * @param node - no a desserializar
+	 * @return plevel - atributo plevel
+	 */
+	public static float getpLevel(Node node)
+	{
+		return Float.valueOf(getNodeValue("plevel", node));
+	}
+	
+	/**
+	 * Initializer getFinalInst() - devolve o atributo finalinst. Se o valor encontrado for negativo lança uma excepção.
+	 * @param node - no a desserializar
+	 * @return finalinst - atributo finalinst
+	 */	
+	public static float getFinalInst(Node node) throws Exception
+	{
+		float finalinst = Float.valueOf(getNodeValue("finalinst", node));		
+		if (finalinst <= 0)
+		{
+			throw new Exception("finalinst must be >= 0");
+		}				
+		return finalinst;
+	}
+	
+	/**
+	 * Initializer getnbNodes() - devolve o atributo nbnodes. 
+	 * @param node - no a desserializar
+	 * @return nbnodes - atributo nbnodes
+	 */		
+	public static int getnbNodes(Node node)
+	{
+		return Integer.parseInt(getNodeValue("nbnodes", node));
+	}
+	
+	/**
+	 * Initializer getNestNode() - devolve o atributo nestnode. 
+	 * @param node - no a desserializar
+	 * @return nestnode - atributo nestnode
+	 */		
+	public static int getNestNode(Node node)
+	{
+		return Integer.parseInt(getNodeValue("nestnode", node)) - 1;
+	}
+	
+	/**
+	 * Initializer getAlpha() - devolve o atributo alpha. 
+	 * @param node - no a desserializar
+	 * @return alpha - atributo alpha
+	 */	
+	public static float getAlpha(Node node)
+	{
+		return Float.valueOf(getNodeValue("alpha", node));
+	}
+	
+	/**
+	 * Initializer getBeta() - devolve o atributo beta. 
+	 * @param node - no a desserializar
+	 * @return beta - atributo beta
+	 */	
+	public static float getBeta(Node node)
+	{
+		return Float.valueOf(getNodeValue("beta", node));
+	}
+	
+	/**
+	 * Initializer getDelta() - devolve o atributo delta. 
+	 * @param node - no a desserializar
+	 * @return delta - atributo delta
+	 */	
+	public static float getDelta(Node node)
+	{
+		return Float.valueOf(getNodeValue("delta", node));
+	}
+	
+	/**
+	 * Initializer getRHO() - devolve o atributo rho. 
+	 * @param node - nó a desserializar
+	 * @return rho - atributo rho
+	 */		
+	public static float getRHO(Node node)
+	{
+		return Float.valueOf(getNodeValue("rho", node));
+	}
+	
+	/**
+	 * Initializer getETA() - devolve o atributo eta. 
+	 * @param node - no a desserializar
+	 * @return eta - atributo eta
+	 */		
+	public static float getETA(Node node)
+	{
+		return Float.valueOf(getNodeValue("eta", node));
+	}
+	
+	/**
+	 * Initializer getNodeIDX() - devolve o atributo nodeidx. Se o valor encontrado for menor que 1 lança uma excepção.
+	 * @param node - no a desserializar
+	 * @return nodeidx - atributo nodeidx
+	 */		
+	public static int getNodeIDX(Node node) throws Exception
+	{
+		int nodeidx = Integer.parseInt(getNodeValue("nodeidx", node));
+		if(nodeidx < 1)
+		{
+			throw new Exception("node must be > 0");
+		}		
+		return nodeidx - 1;
+	}
+	
+	/**
+	 * Initializer getTargetNode() - devolve o atributo targetnode. Se o valor encontrado for menor que 1 lança uma excepção.
+	 * @param node - no a desserializar
+	 * @return targetnode - atributo targetnode
+	 */		
+	public static int getTargetNode(Node node) throws Exception
+	{
+		int targetnode = Integer.parseInt(getNodeValue("targetnode", node));
+		if(targetnode < 1)
+		{
+			throw new Exception("node must be > 0");
+		}		
+		return targetnode - 1;
+	}
+	
+	/**
+	 * Initializer getEdgeWeight() - devolve o atributo weight. 
+	 * @param node - no a desserializar
+	 * @return weight - atributo weight
+	 */		
+	public static int getEdgeWeight(Node node)
+	{
+		return Integer.parseInt(node.getTextContent());
+	}
+	
+	/**
+	 * Initializer getNodeList() - devolve uma lista de nos. 
+	 * @param TagName - no pai da lista
+	 * @param doc - documento XML base
+	 * @return NodeList - lista dos nós encontrados
+	 */		
+	public static NodeList getNodeList(String TagName, org.w3c.dom.Document doc)
+	{
+		return doc.getElementsByTagName(TagName);
+	}
+	
+	/**
+	 * Initializer getNodeValue() - devolve o valor de um no
+	 * @param Name - atributo a desserializar
+	 * @param node - no a desserializar
+	 * @return String - valor do no
+	 */		
+	public static String getNodeValue(String Name, Node node)
+	{
+		return (node.getAttributes().getNamedItem(Name).getNodeValue());
 	}
 	
 }
